a1de34f7f96985629cce812bea9f45f7dd411bca
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2019-07-25 11:29:16 -0400

    Filter Videos

--- api/index.js
+++ api/index.js
@@ -50,12 +50,6 @@ app.get('/playlists/:token/:channel_ref', function(req, res) {
             parser.parseString(body, function(error, result) {
 
                 if(error === null) {
-                    let prueba = result.video_list.map(video => {
-                        return video.date_created;
-                    });
-
-                    console.log(prueba);
-
                     res.send(result)
                 }
                 else {
