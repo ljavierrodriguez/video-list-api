f4e5465ce4eb524963df31089442eb631d67472e
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2019-07-25 14:38:06 -0400

    Filter Videos

--- api/index.js
+++ api/index.js
@@ -52,7 +52,7 @@ app.get('/playlists/:token/:channel_ref', function(req, res) {
                 if(error === null) {
                     let created = 1000*60*60*24;
                     let prueba = result.response.video_list.filter((video) => {
-                        let d1 = new Date(parseInt(video.date_created[0])*1000);
+                        let d1 = new Date(parseInt(video["date_created"])*1000);
                         let d2 = new Date();
                         let total = d2.getTime() - d1.getTime();
                         return Math.floor(total / created) > (req.query.days ? req.query.days : 1);
