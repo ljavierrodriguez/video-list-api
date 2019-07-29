3eefcc2aa58f748fe461596ca65f56f6ef21ba67
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2019-07-29 15:32:32 -0400

    Fix Eliminar Videos en lotes

--- api/index.js
+++ api/index.js
@@ -106,6 +106,39 @@ app.get('/playlists/:token/video/:video_ref/delete', function (req, res) {
     }
 });
 
+app.get('/playlists/:token/videos/delete', function (req, res) {
+    let token = req.params.token;
+    let videos = explode(",", req.query.videos);
+    let preview = req.query.preview;
+
+    let datos = {
+        "details": "videos a eliminar",
+        "count": videos.length,
+        "videos": videos
+    }
+
+    if (preview) {
+        res.send(datos);
+    } else {
+        request({
+            url: "https://www.streamingvideoprovider.com/?l=api&a=svp_delete_video&token=" + token + "&video_ref=" + video_ref,
+            json: false
+        }, function (error, response, body) {
+
+            if (!error && response.statusCode === 200) {
+                // Pintamos la respuesta JSON en navegador.
+                parser.parseString(body, function (error, result) {
+                    if (error === null) {
+                        res.send(result)
+                    } else {
+                        console.log(error);
+                    }
+                });
+            }
+        });
+    }
+});
+
 app.get('/playlists/:token/:channel_ref/delete', function (req, res) {
     let token = req.params.token;
     let channel_ref = req.params.channel_ref
